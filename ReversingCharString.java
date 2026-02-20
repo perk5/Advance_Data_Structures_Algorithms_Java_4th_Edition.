4366d72d4dafd85cf315d2f8ffeb5b1867eab925
Prerak Shah <prerak9359@gmail.com>
2026-02-20 14:05:14 +1030

    Reversed CharString using Recursion.

---
+++ ReversingCharString.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class ReversingCharString {
+
+    public static String reverseString(char[] charArr, int l, int r) {
+        if (l >= r) {
+            String s = new String(charArr);
+            return s;
+        }
+
+        char c = charArr[l];
+        charArr[l] = charArr[r];
+        charArr[r] = c;
+
+        return reverseString(charArr, l + 1, r - 1);
+
+    }
+
+    public static void main(String args[]) {
+
+        String str = "pots&pans";
+        char[] s = str.toCharArray(); 
+        System.out.println(reverseString(s, 0, s.length - 1));
+    }
+}
